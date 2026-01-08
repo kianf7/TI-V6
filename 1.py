698779252880da5eced88591c2b4d5c3a3b5113c
Lukas Ritter <lukas.ritter06@gmail.com>
2026-01-08 20:53:42 +0100

    Add 1, changed name of Aufgabe2

---
+++ 1.py
@@ -0,0 +1,29 @@
+from microbit import *
+
+
+hausnikolaus = [(1,0),(0,1),(0,2),(0,3),(0,4),(1,4),(1,3),
+               (1,2),(2,2),(2,3),(2,4),(3,4),(4,4),(4,3),(4,2),
+               (3,2),(2,2),(2,1),(1,0)]
+
+def blink_led(x,y):
+    display.set_pixel(x,y,9)
+    sleep(100)
+    display.set_pixel(x,y,0)
+    sleep(100)
+    display.set_pixel(x,y,9)
+
+def paint_house():
+    for i in hausnikolaus:
+        if button_b.is_pressed():
+            display.clear()
+            break
+        (x,y) = i
+        blink_led(x,y)
+    display.clear()
+    sleep(1000)
+
+while True:
+    paint_house()
+
+
+    

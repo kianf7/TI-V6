699f161247f130f19851148b1075352a17d68156
Lukas Ritter <lukas.ritter06@gmail.com>
2026-01-08 21:18:09 +0100

    Aufgabe 1 fertig (?)

--- 1.py
+++ 1.py
@@ -1,29 +1,53 @@
 from microbit import *
 
+pixel = [
+    (2,2), (2,3), (2,3), (2,4), (1,3), (0,2), (1,2), (2,2), (1,0), (0,2), (0,3), (0,4), (1,3), (2,2), (3,2),
+    (4,2), (4,3), (4,4), (3,4), (2,4), (1,4), (0,4)
+]
 
-hausnikolaus = [(1,0),(0,1),(0,2),(0,3),(0,4),(1,4),(1,3),
-               (1,2),(2,2),(2,3),(2,4),(3,4),(4,4),(4,3),(4,2),
-               (3,2),(2,2),(2,1),(1,0)]
-
-def blink_led(x,y):
-    display.set_pixel(x,y,9)
-    sleep(100)
-    display.set_pixel(x,y,0)
-    sleep(100)
-    display.set_pixel(x,y,9)
-
-def paint_house():
-    for i in hausnikolaus:
-        if button_b.is_pressed():
+def draw_house():
+    index = 0
+    paused = False
+    
+    while True:
+        # (e) & (g) Button B: Reset / Button A: Pause & Fortsetzen
+        if button_b.was_pressed():
+            index = 0
             display.clear()
-            break
-        (x,y) = i
-        blink_led(x,y)
-    display.clear()
-    sleep(1000)
+        
+        if button_a.was_pressed():
+            paused = not paused
+            if paused:
+                display.show("a")
+            else:
+                # (g) Bild wiederherstellen beim Fortsetzen
+                display.clear()
+                for i in range(index):
+                    px, py = pixel[i]
+                    display.set_pixel(px, py, 9)
 
-while True:
-    paint_house()
+        if not paused:
+            if index < len(pixel):
+                x, y = pixel[index]
+                
+                # (c) LED blinkt einmal kurz auf, bevor sie stabil bleibt
+                display.set_pixel(x, y, 9)
+                sleep(150)
+                display.set_pixel(x, y, 0)
+                sleep(150)
+                display.set_pixel(x, y, 9)
+                
+                index += 1
+                # (b) Geschwindigkeit: ca. 3 LEDs pro Sekunde (300ms + Pause)
+                sleep(100) 
+            else:
+                # (d) Wenn Ende erreicht, von vorne beginnen
+                sleep(1000)
+                index = 0
+                display.clear()
+        
+        sleep(50)
+draw_house()
 
 
     

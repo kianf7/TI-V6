36a05b7c24780eb553d9cc7af1c6138e921280dd
Lukas <lukas.ritter06@gmail.com>
2026-01-09 15:19:15 +0100

    Modify display logic for micro:bit

--- 3c.py
+++ 3c.py
@@ -1,7 +1,10 @@
 from microbit import *
 
-
 while True:
+    for i in range(0, 9):
+        display.set_pixel(2, 2, i)
+        sleep(100)
+    
     for i in range(9, 0, -1):
-        display.set_pixel(3,3,i)
+        display.set_pixel(2, 2, i)
         sleep(100)

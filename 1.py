58acf59a5f52395f8fc8d05056f2b95d098504f5
Kian <kian.fitzkr@gmail.com>
2026-01-09 01:04:52 +0100

    Wenn er ritter heißt

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 from microbit import *
 
 pixel = [
-    (2,2), (2,3), (2,3), (2,4), (1,3), (0,2), (1,2), (2,2), (2,1), (1,0), (0,1),(0,2), (0,3), (0,4), (1,3), (2,2), (3,2),
+    (2,2), (2,3), (2,4), (1,3), (0,2), (1,2), (2,2), (2,1), (1,0), (0,1),(0,2), (0,3), (0,4), (1,3), (2,2), (3,2),
     (4,2), (4,3), (4,4), (3,4), (2,4), (1,4), (0,4)
 ]
 
@@ -27,12 +27,21 @@ def draw_house():
         if not paused:
             if index < len(pixel):
                 x, y = pixel[index]
-                
-                display.set_pixel(x, y, 9)
-                sleep(150)
-                display.set_pixel(x, y, 0)
-                sleep(150)
-                display.set_pixel(x, y, 9)
+
+                if display.get_pixel(x,y) == 9:
+                    display.set_pixel(x, y, 0)
+                    sleep(150)
+                    display.set_pixel(x, y, 9)
+                    sleep(150)
+                    display.set_pixel(x, y, 0)
+                    sleep(150)
+                    display.set_pixel(x,y,9)
+                else:
+                    display.set_pixel(x, y, 9)
+                    sleep(150)
+                    display.set_pixel(x, y, 0)
+                    sleep(150)
+                    display.set_pixel(x, y, 9)
                 
                 index += 1
                 sleep(100) 
